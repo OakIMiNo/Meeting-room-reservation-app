d5bfba871109677cca1b58d336a24dd0dcc191ff
Haruko-Fujita <haru.com.hf@gmail.com>
2023-01-05 09:57:40 +0900

    h_App.tsxに作成したコンポーネンツを追加

--- app/frontend/src/h_App.tsx
+++ app/frontend/src/h_App.tsx
@@ -1,13 +1,44 @@
 import React from "react";
 import "./App.css";
-import ButtonIcon from "./components/ButtonIcon";
-import Login from "./components/Login";
-import ReservedDetails from "./components/ReservedDetails";
-import SelectForm from "./components/SelectForm";
+import { Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import MyPage from "./pages/Mypage";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import { AuthProvider } from "./context/auth";
 
-function App() {
+// Haruko test
+import ButtonIcon from "./ButtonIcon";
+import Login from "./Login";
+import ReservedDetails from "./ReservedDetails";
+import SelectForm from "./SelectForm";
+import { GetReservations } from "./components/GetReservations";
+import { InputDate } from "./components/InputDate";
+import { InputTime } from "./components/InputTime";
+
+const App = (): JSX.Element => {
   return (
     <>
+      {/* ページ作成したら追加する */}
+      <AuthProvider>
+        <header>
+          <Header />
+        </header>
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </AuthProvider>
+
+      {/* Haruko test */}
+      <br />
+      <h1>予約一覧＿作業中</h1>
+      <GetReservations />
+      <br />
+      <InputDate />
+      <InputTime />
+      <br />
       <h1>Booking meeting rooms</h1>
       <p>ログインコンポーネント</p>
       <Login />
@@ -22,6 +53,6 @@ function App() {
       <SelectForm />
     </>
   );
-}
+};
 
 export default App;
